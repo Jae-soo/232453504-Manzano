b935ee065b0d454a275c84d24cfa3f41afc92d5f
root <root@DESKTOP-O15F2GK>
2024-07-19 22:40:59 +0800

    This folder is fighting me.

---
+++ testmysql.php
@@ -0,0 +1,40 @@
+<?php
+phpinfo();
+?>
+
+
+
+<?php
+$servername ="localhost"
+$username = "root";
+$password = "";
+
+$conn = new mysqli ($servername, $username, $password);
+
+if ($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected succsessfully";
+
+?>
+
+<?php
+phpinfo();
+?>
+
+
+
+<?php
+$servername ="localhost"
+$username = "root";
+$password = "";
+
+$conn = new mysqli ($servername, $username, $password);
+
+if ($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected succsessfully";
+
+?>
+
